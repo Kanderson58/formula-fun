f8ecf0c09583235edbc06caaae593dbd861bb027
Kara Anderson <kanderson8585@gmail.com>
2023-04-19 11:46:03 -0600

    Provide sample data for Driver and DriverPage

--- src/utilities.js
+++ src/utilities.js
@@ -15,23 +15,23 @@ export const cleanDriverData = () => {
 }
 
 export const cleanSingleDriver = (name) => {
-  return getData(`drivers?name=${name}`).then(data => data.response.map(driver => {
-   return {
-     name: driver.name,
-     firstName: driver.name.substring(0, driver.name.indexOf(' ')),
-     image: driver.image,
-     number: driver.number,
-     careerPoints: driver.career_points,
-     birthday: driver.birthdate,
-     birthplace: driver.birthplace,
-     country: driver.country.name,
-     totalRaces: driver.grands_prix_entered,
-     totalPodiums: driver.podiums,
-     totalTeams: driver.teams.length,
-     highestFinish: driver.highest_race_finish.position,
-     numHighest: driver.highest_race_finish.number,
-     worldChamp: driver.world_championships
-   }
-  }))
-  // return sampleSingleDriver;
+  // return getData(`drivers?name=${name}`).then(data => data.response.map(driver => {
+  //  return {
+  //    name: driver.name,
+  //    firstName: driver.name.substring(0, driver.name.indexOf(' ')),
+  //    image: driver.image,
+  //    number: driver.number,
+  //    careerPoints: driver.career_points,
+  //    birthday: driver.birthdate,
+  //    birthplace: driver.birthplace,
+  //    country: driver.country.name,
+  //    totalRaces: driver.grands_prix_entered,
+  //    totalPodiums: driver.podiums,
+  //    totalTeams: driver.teams.length,
+  //    highestFinish: driver.highest_race_finish.position,
+  //    numHighest: driver.highest_race_finish.number,
+  //    worldChamp: driver.world_championships
+  //  }
+  // }))
+  return sampleSingleDriver;
 }
