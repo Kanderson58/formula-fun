65d9aedc062be3802f96557ac28c7295a0a36ce2
Kara Anderson <kanderson8585@gmail.com>
2023-04-20 18:06:17 -0600

    Lay out basic main page tests

--- cypress/e2e/racepage.cy.js
+++ cypress/e2e/racepage.cy.js
@@ -1,5 +1,23 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  beforeEach('', () => {
+    cy.intercept('https://v1.formula-1.api-sports.io/rankings/drivers?season=2021', {
+      fixture: 'allDrivers.json'
+    })
+      .visit('http://localhost:3000/')
+  });
+
+  it('starts the user on an instruction page', () => {
+    cy.get('.formula-fun').contains('Welcome to Formula Fun!')
+      .get('p').contains('Get ready to take on the racing world')
+      .get('.see-team').contains('Build My Team');
+  });
+
+  it('allows users to click around each link in the header', () => {
+    cy.get('nav > [href="/team"]').click().get('.default-driver').should('exist');
+    cy.get('[href="/drivers"]').click().get('.full-rankings').should('exist');
+  });
+
+  it('should not allow a user to access their results before they have chosen a team', () => {
+    cy.get('.see-team').click().get('.constructors').should('not.exist');
+  });
 })
