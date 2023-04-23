d6149180b8c22be1af0f9bab8b5bc80aaa2e1ade
Kara Anderson <kanderson8585@gmail.com>
2023-04-22 18:03:43 -0600

    Disallow API calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,10 @@
 export const getData = async(params) => { 
-  const result = await fetch(`https://v1.formula-1.api-sports.io/${params}`, {
-    headers: {
-      'x-apisports-key': 'fbb044d1ec06d67febe82ad0fecb8ff5',
-      "x-rapidapi-host": "v1.formula-1.api-sports.io"
-    }
-  })
-  const drivers = await result.json();
-  return drivers;
+  // const result = await fetch(`https://v1.formula-1.api-sports.io/${params}`, {
+  //   headers: {
+  //     'x-apisports-key': 'fbb044d1ec06d67febe82ad0fecb8ff5',
+  //     "x-rapidapi-host": "v1.formula-1.api-sports.io"
+  //   }
+  // })
+  // const drivers = await result.json();
+  // return drivers;
 }
